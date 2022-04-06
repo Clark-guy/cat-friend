77c78ae4f536807cce508c8aa73f4272d8fa712e
Clark-guy <jputnam1999@gmail.com>
2022-04-05 22:46:50 -0500

    cleaned it all up a little bit. need to condense further

---
+++ blink.ml
@@ -0,0 +1,34 @@
+#load "unix.cma"
+
+let mainTwo = function x ->
+        Printf.printf "\x1b[32;41m%s" "chill";
+        Printf.printf "\x1b[0m\n"
+
+(*define a function to take a filename, return string in that filename*)
+
+let fileToString fname =
+        let channel = open_in fname in
+        let newString = really_input_string channel (in_channel_length channel) in
+        close_in channel;
+        newString;
+;;
+
+
+
+let cat_files = ["cat.txt"; "cat0.txt"];;
+
+let firstCat = fileToString "cats/cat.txt" in
+let secondCat = fileToString "cats/cat0.txt" in
+while true; do
+        Sys.command "clear";
+        print_endline firstCat; 
+        flush stdout;
+        Unix.sleepf(0.5);
+
+        Sys.command "clear";
+        print_endline secondCat; 
+        flush stdout;
+        Unix.sleepf(0.5);
+done;
+
+
